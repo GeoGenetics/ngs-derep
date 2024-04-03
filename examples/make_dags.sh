0bedae96d6870913bf3c56aa5c0a8d96b87f685b
fgvieira <1151762+fgvieira@users.noreply.github.com>
2024-04-03 13:11:33 +0200

    Force all rules for DAGs

--- examples/make_dags.sh
+++ examples/make_dags.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SNAKEMAKE_OPTS="--snakefile ../../workflow/Snakefile --configfile config/config.yaml --printshellcmds --software-deployment-method conda --conda-prefix /tmp/cache/conda $@"
+SNAKEMAKE_OPTS="--snakefile ../../workflow/Snakefile --configfile config/config.yaml --printshellcmds --software-deployment-method conda --conda-prefix /tmp/cache/conda --forceall $@"
 
 for TEST in robot_tests_all robot_tests_extend robot_tests_derep robot_tests_none
 do
